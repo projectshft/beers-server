dd1b12ed26f40345f09033dc4586adc6a217590b
amhayslip <amhayslip@gmail.com>
2019-11-04 14:10:18 -0500

    add require

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var beers = require('./beerRouter')
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/beers');
 
@@ -10,7 +11,7 @@ app.use(function( req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  
+
   next();
 });
 
